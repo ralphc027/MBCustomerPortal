cad8cbe1ebb632918272db1949738101445e7e16
r27 <r27@DESKTOP-FN6H080.local>
2023-09-30 20:51:28 +0800

    Removed @Valid from controller to enable custom error provided by CustomRBodyExceptionHandler for parameters

--- application/src/main/java/com/mb/application/bankservice/service/impl/AppServiceImpl.java
+++ application/src/main/java/com/mb/application/bankservice/service/impl/AppServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 
